aee2c7042d28accc714cc9ef4297ccb7fb64a6d2
vengin <gvengin@gmail.com>
2025-03-18 19:07:40 +0100

    Make the project card itself clickable (instead of clicking on 'Demo' icon)

--- src/components/Projects/SingleProject/SingleProject.js
+++ src/components/Projects/SingleProject/SingleProject.js
@@ -36,81 +36,43 @@ function SingleProject({ id, name, desc, tags, code, demo, image, theme }) {
 
     return (
         <Fade bottom>
-            <div
-                key={id}
-                className='singleProject'
-                style={{ backgroundColor: theme.primary400 }}
-            >
-                <div className='projectContent'>
-                    <h2
-                        id={name.replace(' ', '-').toLowerCase()}
-                        style={{ color: theme.tertiary }}
+            <a href={demo} target="_blank" rel="noopener noreferrer">
+                <div
+                    key={id}
+                    className='singleProject'
+                    style={{ backgroundColor: theme.primary400 }}
+                >
+                    <div className='projectContent'>
+                        <h2
+                            id={name.replace(' ', '-').toLowerCase()}
+                            style={{ color: theme.tertiary }}
+                        >
+                            {name}
+                        </h2>
+                        <img src={image ? image : placeholder} alt={name} />
+                    </div>
+                    <p
+                        className='project--desc'
+                        style={{
+                            background: theme.secondary,
+                            color: theme.tertiary,
+                        }}
                     >
-                        {name}
-                    </h2>
-                    <img src={image ? image : placeholder} alt={name} />
-                    <div className='project--showcaseBtn'>
-                        <a
-                            href={demo}
-                            target='_blank'
-                            rel='noreferrer'
-                            className={classes.iconBtn}
-                            aria-labelledby={`${name
-                                .replace(' ', '-')
-                                .toLowerCase()} ${name
-                                .replace(' ', '-')
-                                .toLowerCase()}-demo`}
-                        >
-                            <FaPlay
-                                id={`${name
-                                    .replace(' ', '-')
-                                    .toLowerCase()}-demo`}
-                                className={classes.icon}
-                                aria-label='Demo'
-                            />
-                        </a>
-                        <a
-                            href={code}
-                            target='_blank'
-                            rel='noreferrer'
-                            className={classes.iconBtn}
-                            aria-labelledby={`${name
-                                .replace(' ', '-')
-                                .toLowerCase()} ${name
-                                .replace(' ', '-')
-                                .toLowerCase()}-code`}
-                        >
-                            <FaCode
-                                id={`${name
-                                    .replace(' ', '-')
-                                    .toLowerCase()}-code`}
-                                className={classes.icon}
-                                aria-label='Code'
-                            />
-                        </a>
+                        {desc}
+                    </p>
+                    <div
+                        className='project--lang'
+                        style={{
+                            background: theme.secondary,
+                            color: theme.tertiary80,
+                        }}
+                    >
+                        {tags.map((tag, id) => (
+                            <span key={id}>{tag}</span>
+                        ))}
                     </div>
                 </div>
-                <p
-                    className='project--desc'
-                    style={{
-                        background: theme.secondary,
-                        color: theme.tertiary,
-                    }}
-                >
-                    {desc}
-                </p>
-                <div
-                    className='project--lang'
-                    style={{
-                        background: theme.secondary,
-                        color: theme.tertiary80,
-                    }}
-                >
-                    {tags.map((tag, id) => (
-                        <span key={id}>{tag}</span>
-                    ))}
-                </div>
-            </div>
+            </a>
         </Fade>
     );
 }

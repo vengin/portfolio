8efb25cab4dfafeea0fd795e16043e8ec3d40c54
vengin <gvengin@gmail.com>
2025-03-20 19:48:06 +0100

    Updated headerData

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
-import resume from '../assets/pdf/resume.pdf'
+import resume from '../assets/pdf/Anton_Makarevich_Lead_FPGA_Embedded_Developer.pdf'
 
 export const headerData = {
-    name: 'Anon Ymous',
+    name: 'Anton Makarevich',
     title: "Lead FPGA/Embedded Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    desciption:"Accomplished FPGA/Embedded Developer with 17 years of commercial experience in FPGA and embedded systems (software, firmware, hardware), working in teams ranging from 2 to 15 members, both remotely and on-site. My primary area of expertise includes a full cycle of FPGA based product development (prototyping, implementation, debugging, verification, delivery). I am also proficient in C/C++ development for various microprocessors/microcontrollers, embedded systems, drivers and desktop applications. Committed to continuous improvement and problem-solving in complex systems.",
+    image: 'https://media.licdn.com/dms/image/v2/D4D35AQH-k55BEMpmng/profile-framedphoto-shrink_200_200/B4DZU2hDB7HIAc-/0/1740376387992?e=1742799600&v=beta&t=nUmFoe6It-va1js5272hmRQvlLxR4jk8SSh-6oCZWdI',
     resumePdf: resume
 }
